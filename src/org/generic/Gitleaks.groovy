bd40877bdbc2e9397faa797d458473c08e9b3457
Suryavanshiaditya1999 <137984887+Suryavanshiaditya1999@users.noreply.github.com>
2024-07-13 01:35:11 +0530

    Delete src/org/generic/Gitleaks.groovy

--- src/org/generic/Gitleaks.groovy
+++
@@ -1,9 +0,0 @@
-package org.generic
-
-def call() {
-    sh '''
-    wget https://github.com/gitleaks/gitleaks/releases/download/v8.18.2/gitleaks_8.18.2_linux_x64.tar.gz
-    tar xvzf gitleaks_8.18.2_linux_x64.tar.gz
-    ./gitleaks detect -v -r CredScanReport
-    '''
-}
