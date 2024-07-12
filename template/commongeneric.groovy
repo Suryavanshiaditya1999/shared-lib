ea961f99a6decda05a43af0d1c4ce0906ed31ada
Suryavanshiaditya1999 <137984887+Suryavanshiaditya1999@users.noreply.github.com>
2024-07-13 01:36:14 +0530

    Update commongeneric.groovy

--- template/commongeneric.groovy
+++ template/commongeneric.groovy
@@ -1,15 +1,7 @@
-// template/commongeneric.groovy
+package template
 
-// Import functions from generic files
-import org.generic.Checkout
-import org.generic.Gitleaks
-import org.generic.TrivyInstaller
+import org.generic.HelloWorld
 
-// Define a method or class that utilizes the imported functions
-def runAll(String url, String creds, String branch, String repoUrl) {
-    Checkout.checkoutRepo(url, creds, branch)
-    Gitleaks.runScan(repoUrl)
-    TrivyInstaller.install()
+def call(script) {
+    HelloWorld.sayHello(script)
 }
-
-return this // This is necessary if commongeneric.groovy is a script
