b09c78d1aa203df7b9df621137183ad5084d63b0
Suryavanshiaditya1999 <137984887+Suryavanshiaditya1999@users.noreply.github.com>
2024-07-13 04:33:22 +0530

    Update commongeneric.groovy

--- vars/commongeneric.groovy
+++ vars/commongeneric.groovy
@@ -1,7 +1,11 @@
 import org.python.CoveragePython
 import org.python.Coverage2
 
-def call() {
-     new CoveragePython().call()
-     new Coverage2.call()
+def call1() {
+    new CoveragePython().call()
+    callCoverage2()
+}
+
+def callCoverage2() {
+    new Coverage2().call()
 }
