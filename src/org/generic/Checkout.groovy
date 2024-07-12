d8470ff2ec570121cdaa6867ae12f2dc15f1897e
Suryavanshiaditya1999 <137984887+Suryavanshiaditya1999@users.noreply.github.com>
2024-07-13 01:35:04 +0530

    Update Checkout.groovy

--- src/org/generic/Checkout.groovy
+++ src/org/generic/Checkout.groovy
@@ -1,10 +1,7 @@
 package org.generic
 
-def call(String url, String creds, String branch) {
-    stage('Clone') {
-        script {
-            echo "Cloning repo ${url} from branch ${branch} using creds ${creds}";
-            git branch: "${branch}", credentialsId: "${creds}", url: "${url}"
-        }
+class HelloWorld {
+    static void sayHello(script) {
+        script.echo "Hello, World!"
     }
 }
